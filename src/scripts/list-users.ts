ef4e2944fee0656848a29492a81c3ff0f072e103
ohwalters <ohwalters@agiledigital.com.au>
2022-08-24 12:35:38 +1000

    IE-4 +Corrected for all linting errors and helped correct some review statements

--- src/scripts/list-users.ts
+++ src/scripts/list-users.ts
@@ -1,41 +1,56 @@
+/* eslint-disable functional/no-conditional-statement */
+/* eslint-disable @typescript-eslint/no-implicit-any-catch */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable functional/no-try-statement */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable functional/immutable-data */
+/* eslint-disable functional/prefer-readonly-type */
+/* eslint-disable functional/no-expression-statement */
 import { Argv } from 'yargs';
 import { RootCommand } from '..';
-import * as okta from "@okta/okta-sdk-nodejs";
+import * as okta from '@okta/okta-sdk-nodejs';
 
-const chalk = require("chalk");
-const chalkTable = require("chalk-table");
+import chalk from 'chalk';
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
+const chalkTable = require('chalk-table');
 
-// User interface, which can be modified for further purposes
-interface user {
-  login: string;
-  email: string;
-  firstName: string;
-  status: string;
-}
+// Subset of user information provided by Okta
+type user = {
+  readonly login: string;
+  readonly email: string;
+  readonly firstName: string;
+  readonly status: string;
+};
 
 // Get the clients given a set of arguments
-async function getClients(clientId: string, privateKey: string): Promise<user[]> {
+async function getUsers(clientId: string, privateKey: string): Promise<user[]> {
   const client = new okta.Client({
-    orgUrl: "https://live-nonprod-esgtech-co.oktapreview.com/",
-    authorizationMode: "PrivateKey",
+    orgUrl: 'https://live-nonprod-esgtech-co.oktapreview.com/',
+    authorizationMode: 'PrivateKey',
     clientId: clientId,
-    scopes: ["okta.users.read"],
-    privateKey:
-      // eslint-disable-next-line no-secrets/no-secrets
-      privateKey
+    scopes: ['okta.users.read'],
+    privateKey: privateKey,
   });
 
-  var users: user[] = [];
+  const users: user[] = [];
 
-  // Adds users to the list of users, can be modified to include additional arguments
-  await client.listUsers().each((user: any) => {
-    users.push({
-      login: String(user.profile.login),
-      email: String(user.profile.email),
-      firstName: String(user.profile.firstName),
-      status: String(user.status)
-    });
-  });
+  // We need to populate users with all of the client data so it can be returned
+  await client
+    .listUsers()
+    .each(
+      (user: {
+        profile: { login: unknown; email: unknown; firstName: unknown };
+        status: unknown;
+      }) => {
+        users.push({
+          login: String(user.profile.login),
+          email: String(user.profile.email),
+          firstName: String(user.profile.firstName),
+          status: String(user.status),
+        });
+      }
+    );
 
   return users;
 }
@@ -43,43 +58,56 @@ async function getClients(clientId: string, privateKey: string): Promise<user[]>
 export default ({ command }: RootCommand): Argv<unknown> =>
   command(
     'list-users',
-    "Provides a list of all users' logins, emails, display names, and statuses. Allows for environment variables under the name OKTAGON_[arg].",
-    (yargs) =>
-      yargs.env("OKTAGON").option(
-        'client-id', {
-        type: 'string',
-        alias: 'cid',
-        describe: 'Okta client ID',
-      }
-      ).option(
-        'private-key', {
-        type: 'string',
-        alias: 'pk',
-        describe: 'Okta private key as string form of JSON',
-      }
-      ).help()
-        .demandOption(['client-id', 'private-key'], "Both arguments are required to sign into Okta"),
-    async (args: any) => {
-
+    // eslint-disable-next-line prettier/prettier
+    'Provides a list of all users\' logins, emails, display names, and statuses. Allows for environment variables under the name OKTAGON_[arg].',
+    (yargs) => {
+      yargs
+        .env('OKTAGON')
+        .option('client-id', {
+          type: 'string',
+          alias: 'cid',
+          describe: 'Okta client ID',
+        })
+        .option('private-key', {
+          type: 'string',
+          alias: 'pk',
+          describe: 'Okta private key as string form of JSON',
+        })
+        .help()
+        .demandOption(
+          ['client-id', 'private-key'],
+          'Both arguments are required to sign into Okta'
+        );
+    },
+    async (args: { clientId: string; privateKey: string }) => {
       try {
-        let clients = await getClients(args.clientId, args.privateKey);
+        const clients = await getUsers(args.clientId, args.privateKey);
 
         const options = {
           leftPad: 1,
           columns: [
-            { field: "login", name: chalk.green("Login") },
-            { field: "email", name: chalk.green("Email") },
-            { field: "firstName", name: chalk.white("D.Name") },
-            { field: "status", name: chalk.yellow("Status") }
-          ]
+            { field: 'login', name: chalk.green('Login') },
+            { field: 'email', name: chalk.green('Email') },
+            { field: 'firstName', name: chalk.white('D.Name') },
+            { field: 'status', name: chalk.yellow('Status') },
+          ],
         };
 
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
         const table = chalkTable(options, clients);
         console.info(table);
-
-      } catch (exp: any) {
-        console.info("\n" + chalk.red.bold("ERROR") + " encountered while performing instruction: " + chalk.green(exp.message));
-        console.info("Please ensure that you have entered the correct parameters into the argument.\n");
+      } catch (error: any) {
+        if (error instanceof Error) {
+          console.error(
+            '\n' +
+              chalk.red.bold('ERROR') +
+              ' encountered while performing instruction: ' +
+              chalk.green(error.message)
+          );
+          console.error(
+            'This is most likely caused by an incorrect private key or client id value inputted either in argument or in environment.\n'
+          );
+        }
       }
     }
   );
