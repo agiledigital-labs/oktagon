969ca3e59b94ad9bb63f5f319e3ff789af1554c4
Daniel Spasojevic <daniel.spasojevic@gmail.com>
2022-10-10 11:53:57 +1100

    IE-30 = Simplified unit tests

--- src/tests/add-user-to-group.test.ts
+++ src/tests/add-user-to-group.test.ts
@@ -1,12 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable unused-imports/no-unused-vars-ts */
 /* eslint-disable functional/no-return-void */
-/* eslint-disable functional/functional-parameters */
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
-
 /* eslint-disable functional/no-expression-statement */
+/* eslint-disable functional/functional-parameters */
 import * as TE from 'fp-ts/TaskEither';
 import * as O from 'fp-ts/Option';
 import { User, UserService } from '../scripts/services/user-service';
@@ -14,10 +8,10 @@ import { addUserToGroup } from '../scripts/add-user-to-group';
 import { Group, GroupService } from '../scripts/services/group-service';
 import { UserStatus } from '@okta/okta-sdk-nodejs';
 
-const returnLeftTE = (_string: string) => TE.left('An error occured');
+const returnLeftTE = jest.fn(() => TE.left('An error occurred'));
 
 const user: User = {
-  id: 'id',
+  id: 'user_id',
   login: 'login',
   email: 'test@localhost',
   name: 'test user',
@@ -25,71 +19,41 @@ const user: User = {
 };
 
 const group: Group = {
-  id: 'id',
+  id: 'group_id',
   name: 'test group',
   type: 'group type',
 };
 
-const baseUserService: UserService = {
+const baseUserService = (): UserService => ({
   getUser: returnLeftTE,
-  createUser: (_x, _y, _z, _w) => returnLeftTE(''),
+  createUser: returnLeftTE,
   deleteUser: returnLeftTE,
   deactivateUser: returnLeftTE,
-  listUsers: () => returnLeftTE(''),
-  isDeactivated: (_user: User) => true,
-};
+  listUsers: returnLeftTE,
+  isDeactivated: () => true,
+});
 
-const baseGroupService: GroupService = {
+const baseGroupService = (): GroupService => ({
   getGroup: returnLeftTE,
-  listGroups: () => returnLeftTE(''),
+  listGroups: returnLeftTE,
   addUserToGroup: returnLeftTE,
   removeUserFromGroup: returnLeftTE,
-};
+});
 
-const baseGetUser = (
-  fail: boolean
-): ((id: string) => TE.TaskEither<string, O.Option<User>>) =>
-  fail ? returnLeftTE : (_id: string) => TE.right(O.some(user));
-
-const baseGetGroup = (
-  fail: boolean
-): ((id: string) => TE.TaskEither<string, O.Option<Group>>) =>
-  fail ? returnLeftTE : (_id: string) => TE.right(O.some(group));
-
-const baseAddUserToGroup = (
-  fail: boolean
-): ((id1: string, id2: string) => TE.TaskEither<string, string>) =>
-  fail ? returnLeftTE : (_groupId: string, _userId: string) => TE.right('blah');
-
-const createUserService = (fail: boolean): UserService => {
-  return {
-    ...baseUserService,
-    getUser: baseGetUser(fail),
-  };
-};
-
-const createGroupService = (
-  getFail: boolean,
-  addFail: boolean
-): GroupService => {
-  return {
-    ...baseGroupService,
-    getGroup: baseGetGroup(getFail),
-    addUserToGroup: baseAddUserToGroup(addFail),
-  };
-};
-
-describe('Adding Users to Groups', () => {
-  const convertToStatement = (str: string) =>
-    // eslint-disable-next-line prettier/prettier
-    str === 'true' ? 'isn\'t valid/doesn\'t exist' : 'is valid/exists';
-
-  it('Expected pass when calling addUserToGroup with an existing and valid user and group', async () => {
+describe('Adding a user to a group', () => {
+  it('passes when calling addUserToGroup with an existing user and group', async () => {
     // Given a user service can retrieve a user.
-    const userService: UserService = createUserService(false);
+    const userService: UserService = {
+      ...baseUserService(),
+      getUser: () => TE.right(O.some(user)),
+    };
 
-    // And a group service that can retrieve a group
-    const groupService: GroupService = createGroupService(false, false);
+    // And a group service that can retrieve a group and add a user to it.
+    const groupService: GroupService = {
+      ...baseGroupService(),
+      getGroup: () => TE.right(O.some(group)),
+      addUserToGroup: jest.fn(() => TE.right('group created')),
+    };
 
     // When we add the user to the group.
     const result = await addUserToGroup(
@@ -100,43 +64,125 @@ describe('Adding Users to Groups', () => {
     )();
 
     // Then we should have a right.
-    expect(result).toBeRight();
+    expect(result).toEqualRight('group created');
+
+    // And the user should have been added to the expected group
+    expect(groupService.addUserToGroup).toBeCalledWith('user_id', 'group_id');
   });
 
-  test.each([
-    [true, true, true],
-    [true, false, true],
-    [false, true, true],
-    [true, true, false],
-    [true, false, false],
-    [false, true, false],
-    [false, false, true],
-  ])(
-    `Expected fail when calling addUserToGroup given the user ${convertToStatement(
-      '%s'
-    )}, group ${convertToStatement(
-      '%s'
-    )}, and addUserToGroup subroutine ${convertToStatement('%s')}`,
-    async (userFail, groupFail, addUserFail) => {
-      // Given a user service can retrieve a user.
-      const userService: UserService = createUserService(userFail);
+  it('fails if the user does not exist', async () => {
+    // Given a user service that can not locate the user.
+    const userService = {
+      ...baseUserService(),
+      getUser: () => TE.right(O.none),
+    };
 
-      // And a group service that can retrieve a group
-      const groupService: GroupService = createGroupService(
-        groupFail,
-        addUserFail
-      );
+    // And a group service that can find the group.
+    const groupService = {
+      ...baseGroupService(),
+      getGroup: () => TE.right(O.some(group)),
+    };
 
-      // When we add the user to the group.
-      const result = await addUserToGroup(
-        userService,
-        groupService,
-        'userId',
-        'groupId'
-      )();
+    // When the user is added to the group
+    const result = await addUserToGroup(
+      userService,
+      groupService,
+      'userId',
+      'groupId'
+    )();
 
-      // Then we should have a left.
-      expect(result).toBeLeft();
-    }
-  );
+    // Then the request should have failed because the user does not exist
+    expect(result).toEqualLeft(
+      'User [userId] does not exist. Cannot add user to group.'
+    );
+
+    // And no attempt was made to add the user to the group.
+    expect(groupService.addUserToGroup).not.toHaveBeenCalled();
+  });
+
+  it('fails if the group does not exist', async () => {
+    // Given a user service that can locate the user.
+    const userService = {
+      ...baseUserService(),
+      getUser: () => TE.right(O.some(user)),
+    };
+
+    // And a group service that can find not the group.
+    const groupService = {
+      ...baseGroupService(),
+      getGroup: () => TE.right(O.none),
+    };
+
+    // When the user is added to the group
+    const result = await addUserToGroup(
+      userService,
+      groupService,
+      'userId',
+      'groupId'
+    )();
+
+    // Then the request should have failed because the group does not exist
+    expect(result).toEqualLeft(
+      'Group [groupId] does not exist. Cannot add user to group.'
+    );
+
+    // And no attempt was made to add the user to the group.
+    expect(groupService.addUserToGroup).not.toHaveBeenCalled();
+  });
+
+  it('fails if both the group and the user do not exist', async () => {
+    // Given a user service that can not locate the user.
+    const userService = {
+      ...baseUserService(),
+      getUser: () => TE.right(O.none),
+    };
+
+    // And a group service that can find not the group.
+    const groupService = {
+      ...baseGroupService(),
+      getGroup: () => TE.right(O.none),
+    };
+
+    // When the user is added to the group
+    const result = await addUserToGroup(
+      userService,
+      groupService,
+      'userId',
+      'groupId'
+    )();
+
+    // Then the request should have failed because the group does not exist
+    expect(result).toEqualLeft(
+      'Group [groupId] does not exist. User [userId] does not exist. Cannot add user to group.'
+    );
+
+    // And no attempt was made to add the user to the group.
+    expect(groupService.addUserToGroup).not.toHaveBeenCalled();
+  });
+
+  it('reports an error when adding to the group failed', async () => {
+    // Given a user service that can locate the user.
+    const userService = {
+      ...baseUserService(),
+      getUser: () => TE.right(O.some(user)),
+    };
+
+    // And a group service that can find the group, but not add the user to it..
+    const groupService = {
+      ...baseGroupService(),
+      getGroup: () => TE.right(O.some(group)),
+      addUserToGroup: () => TE.left('failed to add user to group'),
+    };
+
+    // When the user is added to the group
+    const result = await addUserToGroup(
+      userService,
+      groupService,
+      'userId',
+      'groupId'
+    )();
+
+    // Then the request should have failed with the error from the group service.
+    expect(result).toEqualLeft('failed to add user to group');
+  });
 });
