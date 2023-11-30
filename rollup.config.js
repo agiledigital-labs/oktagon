02323928f9778eb174852ab7a1cdb77eaef03449
Vuthik Kol <vxkol@agiledigital.com.au>
2023-12-01 10:00:03 +1100

    IE-350/reduce-compilation-time = explicitly import and initialise subcommands

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import typescript from 'rollup-plugin-typescript2';
-import * as fg from 'fast-glob';
 import del from 'rollup-plugin-delete';
 import cleanup from 'rollup-plugin-cleanup';
 import { spawnSync } from 'child_process';
@@ -16,29 +15,6 @@ const plugins = [
   cleanup(),
 ];
 
-/**
- * Config to use for the individual script handlers
- *
- * @param {string} filePath
- * @returns {object} rollup configuration
- */
-const scriptConfiguration = (filePath) => ({
-  input: filePath,
-  output: {
-    file: `dist${filePath.replace('src', '').replace(/\.ts$/, '.js')}`,
-    format: 'cjs',
-    exports: 'auto',
-  },
-  plugins,
-});
-
-const scriptFiles = fg.sync(['src/scripts/**/*.ts'], {
-  ignore: [
-    '**/*.test.ts',
-    '**/__fixtures__'
-  ]
-});
-
 export default [
   {
     input: 'src/index.ts',
@@ -54,10 +30,9 @@ export default [
         writeBundle: () => {
           console.info(`make ${cliConfig.name} executable`);
           spawnSync(`chmod`, ['u+x', `dist/${cliConfig.name}`]);
-        }
+        },
       },
       !process.env.ROLLUP_WATCH ? del({ targets: 'dist/**/*' }) : undefined,
     ],
   },
-  ...scriptFiles.map(scriptConfiguration),
 ];
